5d2d659b67facfde1bc69aed9417b6a2d396a96d
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2020-06-19 08:42:03 +0530

    Update session.dart

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:malta/models/subject.dart';
 
 class Session {
   final Subject subject;
   final String image;
 
-  Session({this.subject, this.image});
+  Session({@required this.subject,@required this.image});
 }
