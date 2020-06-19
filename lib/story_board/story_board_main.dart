ebae73ed783fded32100f8b8e1b4acf0acb4efc8
pks99ing <35638587+pks99ing@users.noreply.github.com>
2020-06-19 12:20:26 +0530

    Fixes #6

--- lib/story_board/story_board_main.dart
+++ lib/story_board/story_board_main.dart
@@ -1,29 +1,17 @@
-<<<<<<< HEAD
+
 import 'package:flutter/material.dart';
 import 'package:malta/story_board/story/display_student_test.dart';
-
 import 'package:storyboard/storyboard.dart';
-
-void main() {
-  runApp(
-    MaterialApp(
-      home: Storyboard(
-          [
-            StudentsSessionStory(),
-          ],
-      ),
-=======
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:storyboard/storyboard.dart';
 import 'package:malta/story_board/story/display_school_test.dart';
 
 void main() {
   runApp(MaterialApp(
       home: StoryboardApp([
-        DisplaySchoolStory()
+        DisplaySchoolStory(),
+        StudentsSessionStory(),
       ]),
->>>>>>> 99c4d874310c94bd7de2665df8b7dd355b0c107b
+
     ),
   );
 }
