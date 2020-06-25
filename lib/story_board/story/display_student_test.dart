57b94d2c0f33298563a14f96c37fbd2fea3453f4
pks99ing <35638587+pks99ing@users.noreply.github.com>
2020-06-25 18:19:15 +0530

    getting Students based on sections

--- lib/story_board/story/display_student_test.dart
+++ lib/story_board/story/display_student_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:malta/pages/student_in_class.dart';
+import 'package:malta/pages/students_in_class.dart';
 import 'package:storyboard/storyboard.dart';
 
 class StudentsInClassStory extends Story {
