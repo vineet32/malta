29a11f37724502ff7cecfe82f3adba569e3e1d9b
Mary-Harshitha <62737530+Mary-Harshitha@users.noreply.github.com>
2020-06-26 07:27:22 +0530

    Fixes  #3 Widget test yet to be written

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -12,8 +12,12 @@ class User extends ParseUser implements ParseCloneable {
   User clone(Map<String, dynamic> map) => User.clone()..fromJson(map);
 
   static const String keySchools = 'schools';
+  static const String keyImage = 'image';
 
   List get schools => get<List>(keySchools);
   set schools(List school) => set<List>(keySchools, school);
+
+  String get image => get<String>(keyImage);
+  set image(String image) => set<String>(keyImage, image);
 
 }
