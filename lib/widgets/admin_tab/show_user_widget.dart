19ec0a7b68f6489f5391ae132da682656dc1a8c4
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2020-07-03 15:22:28 +0530

    Update show_user_widget.dart

--- lib/widgets/admin_tab/show_user_widget.dart
+++ lib/widgets/admin_tab/show_user_widget.dart
@@ -12,39 +12,44 @@ class ShowUserWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          ClipOval(
-            child: user.image == null
-                ? Icon(
-                    Icons.person,
-                    size: 100,
-                  )
-                : Image.network(
-                    user.image.url,
-                    width: 100,
-                    height: 100,
-                    colorBlendMode: BlendMode.colorBurn,
-                    fit: BoxFit.cover,
-                    loadingBuilder: (BuildContext context, Widget child,
-                        ImageChunkEvent loadingProgress) {
-                      if (loadingProgress == null) return child;
-                      return Container(
-                        width: 100,
-                        height: 100,
-                        padding: EdgeInsets.all(5),
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                                  loadingProgress.expectedTotalBytes
-                              : null,
-                        ),
-                      );
-                    },
-                  ),
-          ),
-          Text(user.name),
-        ],
+      child: GestureDetector(
+        onTap: () {
+          print("pressed");
+        },
+        child: Column(
+          children: [
+            ClipOval(
+              child: user.image == null
+                  ? Icon(
+                      Icons.person,
+                      size: 100,
+                    )
+                  : Image.network(
+                      user.image.url,
+                      width: 100,
+                      height: 100,
+                      colorBlendMode: BlendMode.colorBurn,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (BuildContext context, Widget child,
+                          ImageChunkEvent loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Container(
+                          width: 100,
+                          height: 100,
+                          padding: EdgeInsets.all(5),
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes
+                                : null,
+                          ),
+                        );
+                      },
+                    ),
+            ),
+            Text(user.name),
+          ],
+        ),
       ),
     );
   }
