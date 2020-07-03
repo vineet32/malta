52dc038c7f74a4970893052bedcf9a42be262597
pks99ing <35638587+pks99ing@users.noreply.github.com>
2020-07-03 13:23:06 +0530

    add Teacher/sponsor/coordinator/principal

--- lib/pages/add_connection.dart
+++ lib/pages/add_connection.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:malta/data/base/api_response.dart';
+import 'package:malta/data/models/connection.dart';
 import 'package:malta/data/models/school.dart';
 import 'package:malta/data/repositories/connection/connection_contract.dart';
+import 'package:malta/providers/school_provider.dart';
 import 'package:malta/widgets/User/show_user.dart';
-
+import 'package:provider/provider.dart';
 
 class AddConnection extends StatefulWidget{
   final String roleType;
@@ -19,6 +22,8 @@ class _AddConnectionState extends State<AddConnection> {
   final FocusNode _searchFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
+    api = Provider.of<ConnectionContract>(context);
+    school=Provider.of<SchoolProvider>(context).getCurrentlySelectedSchool;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -52,11 +57,49 @@ class _AddConnectionState extends State<AddConnection> {
           ),
           foundUser ? Padding(
             padding: const EdgeInsets.only(top: 50),
-            child: ShowUser(userName: _userName,roleType: widget.roleType,),
+            child: _showUser(),
           ) : Text(''),
         ],
       ),
     );
   }
 
+  Widget _showUser() {
+    return FutureBuilder<ApiResponse>(
+        future: api.getUserByUserName(_userName),
+        builder: (BuildContext context, AsyncSnapshot<ApiResponse> snapshot) {
+          if (snapshot.hasData) {
+            if (snapshot.data.results != null) {
+              var userObject=snapshot.data.results[0];
+              return Column(
+                children: [
+                  ShowUser(user: userObject,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      onPressed: (){
+                        api.add(Connection()..set(Connection.keySchool, school)
+                          ..set(Connection.keyRole, widget.roleType)
+                          ..set(Connection.keyUser, userObject));
+                        Navigator.pop(context);
+                      },
+                      child: Text('Add'),
+                      color: Colors.orangeAccent,
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Text('No User Found',style: TextStyle(fontSize: 20,color: Colors.red),);
+            }
+          } else {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.transparent,
+                value: .5,
+              ),
+            );
+          }
+        });
+  }
 }
