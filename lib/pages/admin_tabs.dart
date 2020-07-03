6db8564bf7d56d5eab452f2c1013c9d1e4e1a879
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2020-07-03 08:38:08 +0530

    fixes #43  Administer Bottom Navigation

---
+++ lib/pages/admin_tabs.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:md2_tab_indicator/md2_tab_indicator.dart';
+
+class AdminTabs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          title: TabBar(
+            labelStyle: TextStyle(fontWeight: FontWeight.w700),
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: Color(0xff1967d2),
+            unselectedLabelColor: Color(0xff5f6368),
+            isScrollable: true,
+            indicator: MD2Indicator(
+              indicatorHeight: 3,
+              indicatorColor: Color(0xff1967d2),
+              indicatorSize: MD2IndicatorSize.normal,
+            ),
+            tabs: <Widget>[
+              Tab(
+                text: "School",
+              ),
+              Tab(
+                text: "Sections",
+              ),
+              Tab(
+                text: "Subjects",
+              ),
+              Tab(
+                text: "Principals",
+              ),
+              Tab(
+                text: "Teachers",
+              ),
+              Tab(
+                text: "Coordinators",
+              ),
+              Tab(
+                text: "Sponsors",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+            Icon(Icons.directions_bike),
+            Icon(Icons.directions_bike),
+          ],
+        ),
+      ),
+    );
+
+    // return DefaultTabController(
+    //   length: 7,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: TabBar(
+    //         tabs: [
+    //           Tab(icon: Icon(Icons.directions_car)),
+    //           Tab(icon: Icon(Icons.directions_transit)),
+    //           Tab(icon: Icon(Icons.directions_bike)),
+    //           Tab(icon: Icon(Icons.directions_car)),
+    //           Tab(icon: Icon(Icons.directions_transit)),
+    //           Tab(icon: Icon(Icons.directions_bike)),
+    //           Tab(icon: Icon(Icons.directions_bike)),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: [
+    //         Icon(Icons.directions_car),
+    //         Icon(Icons.directions_transit),
+    //         Icon(Icons.directions_bike),
+    //         Icon(Icons.directions_car),
+    //         Icon(Icons.directions_transit),
+    //         Icon(Icons.directions_bike),
+    //         Icon(Icons.directions_bike),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+}
